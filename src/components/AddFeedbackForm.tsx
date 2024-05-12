893ec91899c7f160800e1abadc1fec139dedc8ad
Hanka8 <hanka.maruskevicova@gmail.com>
2024-05-12 17:57:14 +0200

    feedback form

--- src/components/AddFeedbackForm.tsx
+++ src/components/AddFeedbackForm.tsx
@@ -26,26 +26,29 @@ function AddFeedbackForm(): JSX.Element {
     }
 
     return (
-        <form className='form' onSubmit={addFeedback}>
-            <h2 className='form-title'>Create New Feedback</h2>
-            <label className='label' htmlFor="title">Feedback Title</label>
-            <p className='label-description'>Add a short, descriptive headline</p>
-            <input className='input text' onChange={(e) => setTitle(e.target.value)} type="text" id="title" />
-            <label className='label' htmlFor="category">Category</label>
-            <p className='label-description'>Choose a category for your feedback</p>
-            <select className='input select' onChange={(e) => setCategory(e.target.value as categoryType)} id="category">
-                <option value="bug">Bug</option>
-                <option value="feature">Feature</option>
-                <option value="enhancement">Enhancement</option>
-            </select>
-            <label className='label' htmlFor="detail">Feedback Detail</label>
-            <p className='label-description'>Give more context on your feedback</p>
-            <textarea className='input' onChange={(e) => setDetail(e.target.value)} id="detail"></textarea>
-            <div className='buttons-container'>
-                <button className='btn cancel'>Cancel</button>
-                <button className='btn submit' type='submit'>Add Feedback</button>
-            </div>
-        </form>
+        <>   
+            <form className='form' onSubmit={addFeedback}>
+                <button className='back'>Go Back</button>
+                <h2 className='form-title'>Create New Feedback</h2>
+                <label className='label' htmlFor="title">Feedback Title</label>
+                <p className='label-description'>Add a short, descriptive headline</p>
+                <input className='input text' onChange={(e) => setTitle(e.target.value)} type="text" id="title" />
+                <label className='label' htmlFor="category">Category</label>
+                <p className='label-description'>Choose a category for your feedback</p>
+                <select className='input select' onChange={(e) => setCategory(e.target.value as categoryType)} id="category">
+                    <option value="bug">Bug</option>
+                    <option value="feature">Feature</option>
+                    <option value="enhancement">Enhancement</option>
+                </select>
+                <label className='label' htmlFor="detail">Feedback Detail</label>
+                <p className='label-description'>Give more context on your feedback</p>
+                <textarea className='input' onChange={(e) => setDetail(e.target.value)} id="detail"></textarea>
+                <div className='buttons-container'>
+                    <button className='btn cancel' type='reset'>Cancel</button>
+                    <button className='btn submit' type='submit'>Add Feedback</button>
+                </div>
+            </form>
+        </>
     )
 }
 
