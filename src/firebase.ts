a92d1f8f527abc9e49d97d7636745f26b3a7c4f7
Hanka8 <hanka.maruskevicova@gmail.com>
2024-05-28 09:30:24 +0200

    test3

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,26 +7,26 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAh5AHW4nW0K15GQYLARyljgbNN7RVp99w",
-//   authDomain: "product-feedback-51fef.firebaseapp.com",
-//   projectId: "product-feedback-51fef",
-//   storageBucket: "product-feedback-51fef.appspot.com",
-//   messagingSenderId: "806612704431",
-//   appId: "1:806612704431:web:d86d5b7d4c3566f30f885a",
-//   measurementId: "G-GG15E2R3K7"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAh5AHW4nW0K15GQYLARyljgbNN7RVp99w",
+  authDomain: "product-feedback-51fef.firebaseapp.com",
+  projectId: "product-feedback-51fef",
+  storageBucket: "product-feedback-51fef.appspot.com",
+  messagingSenderId: "806612704431",
+  appId: "1:806612704431:web:d86d5b7d4c3566f30f885a",
+  measurementId: "G-GG15E2R3K7"
+};
 
 // for deployment
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-};
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+// };
 
 
 // Initialize Firebase
